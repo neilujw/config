1e4f1a77f0d1a07e619d3845eae3371a508c6fce
Julien <julien@2a02-8428-1693-c301-a8b4-484c-6069-f1a6.rev.sfr.net>
2025-02-22 20:39:52 +0100

    Bookmarks added

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cp $HOME/.emacs.d/init.el $HOME/config/.emacs.d/
+cp $HOME/.emacs.d/bookmarks $HOME/config/.emacs.d/
 cp $HOME/.config/home-manager/home.nix $HOME/config/.config/home-manager/
