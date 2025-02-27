e8a046935b7ca38438a31afba004107a703d6da2
Julien <julienweber0@gmail.com>
2025-02-27 21:26:51 +0100

    ibuffer groups in another file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,17 +23,7 @@
 
 ; (setq project-mode-line nil)
 
-(setq  ibuffer-saved-filter-groups
-   '(("default"
-      ("f360" (filename . "ws/f360"))
-      ("other elixir" (file-extension . "exs?"))
-      ("emacs"
-       (or (file-extension . "el") (name . "^\\*Messages\\*$")))
-      ("magit" (name . "^magit")))))
-
-(add-hook 'ibuffer-mode-hook
-	  (lambda ()
-	    (ibuffer-switch-to-saved-filter-groups "default")))
+(load-file "ibuffer-groups.el")
 
 (require 'package)
 
--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp $HOME/.emacs.d/init.el $HOME/config/.emacs.d/
+cp $HOME/.emacs.d/ibuffer-groups.el $HOME/config/.emacs.d/
 cp $HOME/.emacs.d/bookmarks $HOME/config/.emacs.d/
 cp $HOME/.config/home-manager/home.nix $HOME/config/.config/home-manager/
